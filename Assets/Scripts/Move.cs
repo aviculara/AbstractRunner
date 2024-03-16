2e8796e16c33c78861c5193c766a789b299be7f1
Lara <114432707+aviculara@users.noreply.github.com>
2024-03-17 01:26:59 +0300

    walls and adjacent area

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,23 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Move : MonoBehaviour
 {
     public GameObject mainCamera;
-    public GameObject ball;
+    
     public GameObject player;
     public GameObject portal;
     public float speed = 10;
+    public Positions position;
+
     private int rotation;
     private float pi = 3.14159f;
     private float x;
+    private bool moving = false;
+    /*
+    private bool onLeft = false;
+    private bool onRight = false;
+    private bool onMid = false;
+    */
     // Start is called before the first frame update
     void Start()
     {
-
+        position = Positions.OnMid;
     }
-
+    public enum Positions
+    {
+        OnMid,
+        OnLeft,
+        OnRight
+    }
     // Update is called once per frame
     void Update()
     {
@@ -33,13 +47,54 @@ public class Move : MonoBehaviour
         portal.transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * 60);
         x = 0;
         mainCamera.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && position != Positions.OnLeft  && !moving)
         {
-            x = 4;
+            if(position == Positions.OnMid)
+            {
+                position = Positions.OnLeft;
+            }
+            else if (position == Positions.OnRight)
+            {
+                position = Positions.OnMid;
+            }
+            transform.DOMoveX(transform.position.x - 4, 0.25f).OnComplete(stopMove);
+            moving = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.D) && position != Positions.OnRight && !moving)
+        {
+            if (position == Positions.OnMid)
+            {
+                position = Positions.OnRight;
+            }
+            else if (position == Positions.OnLeft)
+            {
+                position = Positions.OnMid;
+            }
+            transform.DOMoveX(transform.position.x + 4, 0.25f).OnComplete(stopMove);
+            moving = true;
         }
 
-        player.transform.Translate(new Vector3(x, 0, 1) * Time.deltaTime * speed);
-        ball.transform.Rotate(new Vector3(1, 0, x) * Time.deltaTime * speed * 360 / (pi * 2.5f));
+        player.transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+        transform.Rotate(new Vector3(1, 0, x) * Time.deltaTime * speed * 360 / (pi * 2.5f));
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        /*
+        if (other.tag == "FinishLine")
+        {
+            speed = 0;
+        }
+        if (other.tag == "CheckPoint")
+        {
+            speed *= 2;
+        }
+        */
+        speed = 0;
+    }
+
+    private void stopMove()
+    {
+        moving = false;
     }
 }
